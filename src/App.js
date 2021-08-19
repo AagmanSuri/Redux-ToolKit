e0193cc23bf09a6842a803750b530dbf906ca568
Aagman Suri <aagmansuri96@gmail.com>
2021-08-20 02:56:44 +0530

    Redux toolkit , useSelector,useDispatch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
 import './App.css';
-
+import {useSelector,useDispatch} from 'react-redux'
+import { decrement, increment,incrementByAmount } from './redux/counter';
 function App() {
+   const {value} = useSelector(state => state.counter)
+  const dispatch = useDispatch();
   return (
     <div className="App">
-<h1>hELlo</h1>
+      <h1>Counter : {value}</h1>
+      <button onClick={()=>{dispatch(increment())}}>increment</button>
+      <button onClick={()=>{dispatch(decrement())}}>decrement</button>
+      <button onClick={()=>{dispatch(incrementByAmount(10))}}>Add 10</button>
     </div>
   );
 }
